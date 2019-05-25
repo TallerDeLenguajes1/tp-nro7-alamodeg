d0342231ac8f1d810abcc06252ccf93b56e30d8e
Alvaro Deguer <alvarodeguer@gmail.com>
2019-05-25 20:59:15 -0300

    Fase inicial de Listas

---
+++ Listas/Ejercicio/Empleado.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio
+{
+    //ENUMERACIONES
+    public enum Cargo { Auxiliar, Administrativo, Ingeniero, Especialista, Investigador };
+    public enum EstadoCivil { Soltero, Casado };
+    public enum Genero { Masculino, Femenino };
+   
+    //CLASE Y CONSTRUCTOR EMPLEADO//
+    public class Empleado
+    {
+        public string Nombre;
+        public string Apellido;
+        public DateTime FechaNacimiento;
+        public DateTime FechaIngreso;
+        public Cargo cargo;
+        public EstadoCivil estadoCivil;
+        public Genero genero;
+        public float Sueldo;
+
+        public Empleado(string _nombre, string _apellido, DateTime _fechaNacimiento, DateTime _fechaIngreso, float _sueldo, Cargo _cargo, EstadoCivil _estadoCivil, Genero _genero)
+        {
+            this.Nombre = _nombre;
+            this.Apellido = _apellido;
+            this.FechaNacimiento = _fechaNacimiento;
+            this.FechaIngreso = _fechaIngreso;
+            this.Sueldo = _sueldo;
+            this.cargo = _cargo;
+            this.FechaNacimiento = _fechaNacimiento;
+            this.FechaIngreso = _fechaIngreso;
+        }
+    }
+}
---
+++ Listas/Ejercicio/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Empleado> ListaEmpleados = new List<Empleado>();
+            string aux_nombre;
+            string aux_apellido;
+            DateTime aux_fechaNacimiento;
+            DateTime aux_fechaIngreso;
+            Cargo aux_cargo;
+            EstadoCivil aux_estadoCivil;
+            Genero aux_genero;
+            float aux_sueldo;
+
+
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Ingrese nombre");
+                aux_nombre = Console.ReadLine();
+                Empleado Empleado1 = new Empleado(aux_nombre,);
+                ListaEmpleados.Add(Empleado1);
+            }
+
+           /*Console.Write("escriba apellido ");
+            Empleado1.Apellido = Console.ReadLine();
+            Console.Write(Empleado1.Apellido);
+
+            Console.ReadKey();*/
+            }
+    }
+}
+
